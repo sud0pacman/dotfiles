b8e63de996061e94308b5c22da665001fa57c716
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-02-25 03:31:24 +0000

    feat: add bootloader config

--- configuration.nix
+++ configuration.nix
@@ -70,6 +70,38 @@
   };
 
   programs.adb.enable = true;
+  
+  # Boot loader
+  boot = {
+    loader = {
+      # After going back to bootloader, run this: 
+      # nixos-rebuild switch --install-bootloader
+      systemd-boot.enable = false;
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        devices = ["nodev"];
+        useOSProber = true;
+        efiSupport = true;
+        theme = "${
+          (pkgs.fetchFromGitHub {
+            owner = "xinux-org";
+            repo = "bootloader-theme";
+            tag = "v1.0.3";
+            hash = "sha256-ipaiJiQ3r2B3si1pFKdp/qykcpaGV+EqXRwl6UkCohs=";
+          })
+        }/xinux";
+      };
+    };
+    # plymouth = {
+    #   enable = true;
+    #   theme = "mac-style";
+    #   themePackages = [inputs.mac-style-plymouth.packages."${pkgs.stdenv.hostPlatform.system}".default];
+    # };
+    consoleLogLevel = 3;
+    initrd.systemd.enable = true;
+    initrd.verbose = false;
+  };
 
   # remove all gnome utility apps
   # services.gnome.core-utilities.enable = false;
